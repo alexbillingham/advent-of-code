5c7bbd46c26a3758bebefb2f0d776865455c25a3
Alex Billingham <ajb1997@hotmail.com>
2024-12-12 17:51:11 +0000

    Refactor day 1

--- 2024/day_1/Program.cs
+++ 2024/day_1/Program.cs
@@ -6,57 +6,74 @@ namespace advent_of_code_day_1
         const string PUZZLE_INPUT = "puzzle_input.txt";
         static void Main(string[] args)
         {
-            // Read contents
-            var contents = File.ReadAllLines(PUZZLE_INPUT).ToList();
+            LocationLists locationLists = ParseInput(PUZZLE_INPUT);
 
-            // Define both lists
-            var left_list = new List<int>();
-            var right_list = new List<int>();
+            // Sort lists
+            locationLists.leftList.Sort();
+            locationLists.rightList.Sort();
+
+            // Confirm both lists are equal length (probably dont need to do this oh well)
+            if (locationLists.leftList.Count() != locationLists.rightList.Count())
+            {
+                Console.WriteLine($"Lists dont equal size. {locationLists.leftList.Count()} != {locationLists.rightList.Count()}");
+                return;
+            }
+
+            DistanceBetweenLists(locationLists);
+            SimilarityScore(locationLists);
+        }
+
+        class LocationLists
+        {
+            public List<int> leftList { get; set; }
+            public List<int> rightList { get; set; }
+
+            public LocationLists()
+            {
+                leftList = new List<int>();
+                rightList = new List<int>();
+            }
+        }
+
+        static LocationLists ParseInput(string filePath)
+        {
+            // Read contents
+            var contents = File.ReadAllLines(filePath).ToList();
+
+            var locationLists = new LocationLists();
 
             // Split the puzzle input into both lists
             foreach (string item in contents)
             {
-                var split_item = item.Split("   ");
-                left_list.Add(int.Parse(split_item[0]));
-                right_list.Add(int.Parse(split_item[1]));
+                var splitItem = item.Split("   ");
+                locationLists.leftList.Add(int.Parse(splitItem[0]));
+                locationLists.rightList.Add(int.Parse(splitItem[1]));
             }
 
-            // Sort lists
-            left_list.Sort();
-            right_list.Sort();
-
-            // Confirm both lists are equal length (probably dont need to do this oh well)
-            if (left_list.Count() != right_list.Count())
-            {
-                Console.WriteLine($"Lists dont equal size. {left_list.Count()} != {right_list.Count()}");
-                return;
-            }
-
-            DistanceBetweenLists(left_list, right_list);
-            SimilarityScore(left_list, right_list);
+            return locationLists;
         }
 
-        static void DistanceBetweenLists(List<int> left_list, List<int> right_list)
+        static void DistanceBetweenLists(LocationLists locationLists)
         {
             // Define total variable
             int result = 0;
 
             // Loop through the left list and work out the distance to the right list
-            for (int i = 0; i < left_list.Count(); i++)
+            for (int i = 0; i < locationLists.leftList.Count(); i++)
             {
-                result += Math.Abs(left_list[i] - right_list[i]);
+                result += Math.Abs(locationLists.leftList[i] - locationLists.rightList[i]);
             }
 
             Console.WriteLine($"Distance between lists = {result}");
         }
 
-        static void SimilarityScore(List<int> left_list, List<int> right_list)
+        static void SimilarityScore(LocationLists locationLists)
         {
             int result = 0;
 
-            for (int i = 0; i < left_list.Count(); i++)
+            for (int i = 0; i < locationLists.leftList.Count(); i++)
             {
-                result += left_list[i] * right_list.Count(location_id => location_id == left_list[i]);
+                result += locationLists.leftList[i] * locationLists.rightList.Count(location_id => location_id == locationLists.leftList[i]);
             }
 
             Console.WriteLine($"List similarity score = {result}");
